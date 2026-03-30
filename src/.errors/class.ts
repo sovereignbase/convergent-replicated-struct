e9fe2c8fbd9ffb1305527a5eca2404526d9896c5
Jori Lethinen <lehtinenjori03@gmail.com>
2026-03-30 14:31:53 +0300

    chore: getting started

--- src/.errors/class.ts
+++ src/.errors/class.ts
@@ -1,12 +1,12 @@
-export type PackageNameErrorCode = 'EXAMPLE_ERROR_CODE'
+export type ORMapErrorCode = ''
 
-export class PackageNameError extends Error {
-  readonly code: PackageNameErrorCode
+export class ORMapError extends Error {
+  readonly code: ORMapErrorCode
 
-  constructor(code: PackageNameErrorCode, message?: string) {
+  constructor(code: ORMapErrorCode, message?: string) {
     const detail = message ?? code
-    super(`{@z-base/package-name} ${detail}`)
+    super(`{@sovereignbase/observed-remove-map} ${detail}`)
     this.code = code
-    this.name = 'PackageNameError'
+    this.name = 'ORMapError'
   }
 }
